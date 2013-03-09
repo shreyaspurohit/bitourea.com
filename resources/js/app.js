d499b30b71346ba41c2cf26729535fe67b47c4af
Shreyas Purohit <shreyas.purohit@gmail.com>
2013-03-09 02:26:24 -0600

    Added all the projects and other sections

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,39 @@
+function onDialogSetMinWidth(dialogVal){								
+	$(dialogVal).dialog("option", 'minWidth', 500);
+	$(dialogVal).dialog("option", 'minHeight', 400);				
+}
 
+function loadTumblrIframe(){
+	$("#newsThoughtsDiv iframe").get(0).contentWindow.location.href = "http://shreyaspurohit.tumblr.com";
+}
+
+function unloadTumblrIframe(){
+	$("#newsThoughtsDiv iframe").get(0).contentWindow.location.href = "about:blank";
+}
+
+function showDialog(type, widthValPerc, heightValPerc, hideTitle, onComplete){
+	var w = widthValPerc ? widthValPerc : 0.5;
+	var h = heightValPerc ? heightValPerc : 0.75;
+	var wWidth = $(window).width();
+	var dWidth = wWidth * w;
+	var wHeight = $(window).height();
+	var dHeight = wHeight * h;
+	
+	$(type).dialog({
+	  dialogClass: "no-close" + (hideTitle === true ? " noTitle" : " withTitle"),
+	  modal: true,
+	  buttons: {
+		Close: function() {
+		  $( this ).dialog( "close" );
+		}
+	  },
+	  draggable: false,
+	  width:dWidth
+	});				
+	if(heightValPerc){
+		$(type).dialog("option", "height", dHeight);
+	}
+	if(onComplete){
+		onComplete(type);
+	}
+}
